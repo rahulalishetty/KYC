69894705391d0ea09d1c247def29b7946303ceaa
rahulalishetty <rahulalishetty1905@gmail.com>
2019-01-29 12:35:48 +0530

    cleaned code

--- test/test/ValidRangeFinderTest.java
+++ test/test/ValidRangeFinderTest.java
@@ -1,4 +1,4 @@
-
+package test;
 
 import main.ValidRangeFinder;
 import org.junit.jupiter.api.Test;
@@ -8,14 +8,13 @@ import static org.junit.Assert.*;
 
 public class ValidRangeFinderTest {
 
-    static ValidRangeFinder validRangeFinder=new ValidRangeFinder();
 
     @Test
     public void testValidCase() {
         //when current date is between range
         String signUpDate="16-07-1998";
         String currentDate= "27-06-2017";
-        String result = validRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
+        String result = ValidRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
         assertEquals("16-06-2017 27-06-2017", result);
     }
 
@@ -24,7 +23,7 @@ public class ValidRangeFinderTest {
         //when the current date is before signupdate
         String signUpDate="04-05-2017";
         String currentDate= "04-04-2017";
-        String result = validRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
+        String result = ValidRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
         assertEquals("", result);
     }
 
@@ -33,7 +32,7 @@ public class ValidRangeFinderTest {
         //when current date is before rangeStartDate
         String signUpDate="16-07-1998";
         String currentDate= "15-06-2017";
-        String result = validRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
+        String result = ValidRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
         assertEquals("", result);
     }
 
@@ -42,11 +41,8 @@ public class ValidRangeFinderTest {
         //when current date is after rangeEndDate
         String signUpDate="04-02-2016";
         String currentDate= "04-04-2017";
-        String result = validRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
+        String result = ValidRangeFinder.assignSignUpAndCurrentDates(signUpDate, currentDate);
         assertEquals("05-01-2017 06-03-2017", result);
     }
-
-
-
 
 }
